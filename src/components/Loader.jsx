154906cf29ad3c4cd81dda1f69403cf73fb29cbe
t0nqwer <oottoonnoo@gmail.com>
2024-04-20 02:26:22 +0700

    Add necessary configuration files and initial code files

---
+++ src/components/Loader.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Loader = () => {
+  return (
+    <div className=" absolute top-0 left-0 w-full h-full flex justify-center items-center">
+      <div className="w-[10vw] h-[10vw] rounded-full">Loading ....</div>
+    </div>
+  );
+};
+
+export default Loader;
